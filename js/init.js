da9e1fd272fc084fb30a5db50c83d565e8150d8a
Hari G <ghari81189@gmail.com>
2018-06-22 11:25:17 +0530

    Added complete files

--- js/init.js
+++ js/init.js
@@ -1,3 +1,30 @@
 $(document).ready(() => {
-  console.log('asdfasdf');
+  $('.button-collapse').sideNav(); 
+  $('.parallax').parallax();
+  $('.collapsible').collapsible();
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+  $('.materialboxed').materialbox();
+  $('.scrollspy').scrollSpy();
+  $('#contact').scrollfire({
+    // Offsets
+    offset: 100,
+    topOffset: 100,
+    bottomOffset: 100,
+    // Fires once when element begins to come in from the top
+    onBottomIn: function( elm, distance_scrolled ) {
+      setTimeout(() => {
+        Materialize.toast("Please feel free to contact for feedback", 2500 );
+      }, 1000);
+      setTimeout(() => {
+        Materialize.toast("Or to just say hello..", 2500 );
+      }, 4000);
+      $('#contact').scrollfire('remove');
+    }
+  });
+  setTimeout(() => {
+    Materialize.toast("Hey!", 2500 );
+  }, 1000);
+  setTimeout(() => {
+    Materialize.toast("Welcome..", 2500 );
+  }, 4000);
 });
