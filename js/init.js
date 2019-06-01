af9fd806d7c46b4a86e00fb5dfd6bfb811e6099b
Hari G <ghari81189@gmail.com>
2019-06-01 15:28:24 +0530

    profile image orientated rotation

--- js/init.js
+++ js/init.js
@@ -13,7 +13,7 @@ $(document).ready(() => {
     // Fires once when element begins to come in from the top
     onBottomIn: function( elm, distance_scrolled ) {
       setTimeout(() => {
-        Materialize.toast("Please feel free to contact for feedback", 2500, 'toast-color' );
+        Materialize.toast("Please feel free to contact for feedback", 2500, 'toast-mobile' );
       }, 1000);
       setTimeout(() => {
         Materialize.toast("Or to just say hello..", 2500, 'toast-mobile' );
@@ -27,4 +27,15 @@ $(document).ready(() => {
   setTimeout(() => {
     Materialize.toast("Welcome..", 2500, 'toast-mobile' );
   }, 4000);
+
+  const imageDiv = document.getElementById('image');
+  let isInitial = true;
+  window.addEventListener('deviceorientation', (event) => {
+    if (isInitial) {
+      isInitial = false;
+      initialValue = event.alpha;
+    }
+    imageDiv.style.transform = `rotateZ(${event.alpha-initialValue + 180}deg)`;
+  });
+
 });
